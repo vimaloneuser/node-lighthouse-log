59191691e43aad8a2d987dbd4c71b6af1aa9e095
Vimal Patel <vimalp@itpathsolutions.in>
2022-05-10 13:16:05 +0530

    env log is added

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 const axios = require("axios");
 
 exports.Log = ({
-    vp,
-    vd,
-    logType,
-    ip,
-    message,
-    filename,
-    lineNumber,
-    headers = {},
-    endPoint,
-    method,
-    tags
+    error, method, endPoint, requestIp, headers, logType
 }) => {
     return new Promise((resolve, reject) => {
-        if (!vp || !vd || !logType || !endPoint || !method || !message || !filename || !lineNumber) {
-            reject("These are required proprties vp , vd , logType , log , endPoint , method , message , filename , lineNumber");
+        if (!process.env.PROJECT_KEY || !process.env.DELIVERABLE_KEY)
+            reject("PROJECT_KEY or DELIVERABLE_KEY is not defined in .env file");
+        if (!error || !endPoint || !method || !requestIp) {
+            reject("Missing 'error' or 'method' or 'endPoint' or 'requestIp' ");
             return;
         }
+        const [filename, line, column] = error.stack.match(/\/([\/\w-_\.]+\.js):(\d*):(\d*)/);
         const body = {
-            vp,
-            vd,
-            o: logType,
+            vp: process.env.PROJECT_KEY,
+            vd: process.env.DELIVERABLE_KEY,
+            o: logType ? logType : 5,
             a: JSON.stringify({
                 b: {
-                    c: message,
+                    c: error?.message,
                     d: filename,
-                    f: lineNumber,
+                    f: column,
                     g: 0,
                     h: "Error",
                 },
+                i: error.stack,
                 k: headers,
                 m: endPoint,
                 n: method,
-                j: tags
+                j: process.versions
             }),
-            r: ip
+            r: requestIp
         }
 
         axios.post("http://192.168.1.152/lighthouseLumen/public/add-log", body, {
